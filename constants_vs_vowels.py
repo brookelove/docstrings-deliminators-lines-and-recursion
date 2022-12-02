0cf2d0a145bcc5f1aaa0c332354c0076738abb27
brookelove <brookelove@Brookes-iMac-2.local>
2022-12-01 21:34:34 -0800

    passing info to the correct function

--- constants_vs_vowels.py
+++ constants_vs_vowels.py
@@ -2,27 +2,29 @@ import string
 
 
 VOWELS = ['A', 'E', 'I', 'O', 'U']
-vowel_dict = {}
-consonant_dict = {}
+v_lst = []
+c_lst = []
 sentence_lst = []
 
 # counts the letters out of the list and put it in vowel dictionary
 
 
-def vowel_counter():
+def vowel_counter(line):
     # counts the letter of consonants and put in in the dictionary
-    pass
+    print(line)
+    # pass
 
 
-def consonant_counter():
+def consonant_counter(line):
+    print(line)
     # relies on the counter wil be raising errors
-    pass
+    #  pass
 
 
 def vc_counter(filename):
     for lines in filename:
-        print(lines)
-    return lines
+        vowel_counter(lines)
+        consonant_counter(lines)
 
 # def get_file_input():
 #     done = False
@@ -34,13 +36,15 @@ def read_file():
     # main function where the try and catch that will read the file and try to run vc_counter function
     # complete = False
     while True:
-        file_input = input("Enter a text file: ")
+        file_input = input("Enter the name of a text file: ")
         try:
             # with function automatically closes file
             with open(f"{file_input}.txt", "r") as f:
                 vc_counter(f)
-                break
-                # complete = True
+            print(f"Total # of vowels in the text file: {v_lst}")
+            print(f"Total # of consonants in text file: {c_lst}")
+            break
+            # complete = True
         except FileNotFoundError as e:
             print("Could not find file, please try again: ", e)
             continue
