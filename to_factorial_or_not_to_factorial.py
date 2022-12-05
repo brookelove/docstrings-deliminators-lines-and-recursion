f5dd154a53cad01a5462dbdeedcce0e680d90414
brookelove <brookelove@Brookes-iMac-2.local>
2022-12-04 20:27:48 -0800

    functions of factorial and recusion

--- to_factorial_or_not_to_factorial.py
+++ to_factorial_or_not_to_factorial.py
@@ -10,11 +10,24 @@ class NotAPositiveError(Exception):
 
 
 def factorial(num):
-    print("factorial")
+    # print("factorial") # check to see if we are in the correct area
+    # int_num = int(num)
+    if num == 1:
+        return num
+    else:
+        # print(int_num * factorial(int_num - 1))
+        # print(2*3)
+        # fact_result = (int_num * factorial(int_num - 1))
+        # print(fact_result)
+        return num * factorial(num - 1)
 
 
 def factorial2(num):
-    print("factorial 2")
+    result = 1
+    for i in range(1, num+1):
+        # result *= i
+        result = result * i
+    return result
 
 
 # def get_user_input():
@@ -26,6 +39,10 @@ def factorial2(num):
 #         raise NotAnIntegerError
 #     factorial(int(user_num))
 #     factorial2(int(user_num))
+def fact_results(f1, f2):
+    print(f"The results of the factorial using a recursive function are: {f1}")
+    print(
+        f"The results of the factorial using a non-recursive function are: {f2}")
 
 
 def run_factoirals():
@@ -36,8 +53,10 @@ def run_factoirals():
                 input("Please input a number that you would like to find the factorial of: "))
             if user_num < 0:
                 raise NotAPositiveError
-            factorial(int(user_num))
-            factorial2(int(user_num))
+            done = True
+            f1 = factorial(int(user_num))
+            f2 = factorial2(int(user_num))
+            fact_results(f1, f2)
         except ValueError:
             print("The value you printd is not an integer please try again")
         except NotAPositiveError:
